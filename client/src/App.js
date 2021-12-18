f43be0b49bfa2183833639d31368d6f44583b318
Daniel Lousa <dlousa92@gmail.com>
2021-12-18 18:01:44 -0500

    store sortedItem value for use with arrow direction

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,27 @@ import api from "./api";
 const App = () => {
 	// state variable to hold the records from the json-server server.
 	const [records, setRecords] = useState();
+	const [sortedItem, setSortedItem] = useState(api.DATA_PROPS[0]);
 
-	// // use api.GET method to fetch the records from the server on component mount
+	// // use api.GET method to fetch the records from the server on component mount and whenever a user sorts by new category
 	useEffect(() => {
-		const data = api.GET(api.DATA_PROPS[0], api.ORDERS[0]);
+		const data = api.GET(sortedItem, api.ORDERS[0]);
 		data.then((response) => response.json()).then((data) => setRecords(data));
-	}, []);
+	}, [sortedItem]);
 
-	const handleTitleClick = (value) => {
+	const handleTitleClick = (event) => {
 		// Change the order of the items when the head row item of the table gets clicked.
 		// if a column title is clicked, the records should get ordered by that key in an ascending way
 		// if it is clicked again it should toggle the sort order to be descending
-		console.log("i was clicked");
+
+		setSortedItem(event.target.innerText.toLowerCase());
 	};
 
 	const handleOpenInfo = (record) => {
 		// Open a popup displaying the record information
 	};
+
+	console.log(sortedItem);
 
 	return (
 		<div className="container">
@@ -33,6 +37,7 @@ const App = () => {
 				onTitleClick={handleTitleClick}
 				onOpenInfoClick={handleOpenInfo}
 				records={records}
+				sortedItem={sortedItem}
 			/>
 		</div>
 	);
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -7,17 +7,27 @@ import {
 	faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Table = ({ records, onTitleClick, onOpenInfoClick }) => {
-	console.log(records);
+const Table = ({ records, onTitleClick, onOpenInfoClick, sortedItem }) => {
 	return (
 		<table style={{ width: 1000 }}>
 			<thead>
 				<tr>
 					{api.DATA_PROPS.map((head) => (
 						<th style={{ width: 100 }}>
-							{head}
-							<button onClick={onTitleClick}>
-								<FontAwesomeIcon icon={faArrowRight} />
+							<button
+								onClick={onTitleClick}
+								style={{
+									backgroundColor: "transparent",
+									border: "none",
+									outline: "none",
+								}}
+							>
+								<h5>{head.toUpperCase()}</h5>
+								{sortedItem === head ? (
+									<FontAwesomeIcon icon={faArrowDown} />
+								) : (
+									""
+								)}
 							</button>
 						</th>
 					))}
@@ -34,7 +44,6 @@ const Table = ({ records, onTitleClick, onOpenInfoClick }) => {
 };
 
 const TableRow = ({ record }) => {
-	console.log(record);
 	return (
 		<tr>
 			{Object.keys(record).map((val) => (
